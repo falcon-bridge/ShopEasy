3533cb824fdf6803131d76490b98bb5236af96a6
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-03-15 18:02:36 +0530

    created a product model and created routes for viewing all products and creating a new product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,23 @@
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: "Route is working fine" });
+const Product = require("../models/productModel");
+
+//Create product -- admin
+
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+//get all products
+
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
 };
