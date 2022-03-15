90697868e7462350da76dbf4807613b9ce9d9418
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-03-15 18:21:53 +0530

    created route for updating a product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,16 @@
 const Product = require("../models/productModel");
 
+//get all products
+
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
+
 //Create product -- admin
 
 exports.createProduct = async (req, res, next) => {
@@ -11,13 +22,26 @@ exports.createProduct = async (req, res, next) => {
   });
 };
 
-//get all products
+//updating a product -- admin
 
-exports.getAllProducts = async (req, res) => {
-  const products = await Product.find();
+exports.updateProduct = async (req, res, next) => {
+  let product = Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product no found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
 
   res.status(200).json({
     success: true,
-    products,
+    product,
   });
 };
