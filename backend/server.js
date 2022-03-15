622b3ad6e4a9d09bdf07a1b79f8a7e595303fa2f
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-03-15 17:00:17 +0530

    connection to database is set with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,15 @@ const app = require("./app");
 
 const dotenv = require("dotenv");
 
+const connectDatabase = require("./config/database");
+
 //config
 
 dotenv.config({ path: "backend/config/config.env" });
+
+//connecting to database
+
+connectDatabase();
 
 const port = process.env.PORT;
 
