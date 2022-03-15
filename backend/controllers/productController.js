ee8c028759f45fe7afce2a6a1817e6c2afad7495
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-03-15 18:53:31 +0530

    created a route for deleting a product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -45,3 +45,23 @@ exports.updateProduct = async (req, res, next) => {
     product,
   });
 };
+
+// Delete a product -- admin
+
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  await product.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Product deleted successfully",
+  });
+};
