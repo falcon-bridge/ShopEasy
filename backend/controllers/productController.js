9727cb94bdb8c41ac03d799f74bc434eb971324c
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-03-22 16:41:36 +0530

    made a custom error class for handling errors when product isn't found

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,7 @@
 const Product = require("../models/productModel");
 
+const ErrorHandler = require("../utils/errorHandler");
+
 //get all products
 
 exports.getAllProducts = async (req, res) => {
@@ -17,10 +19,11 @@ exports.getProductDetails = async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product not found",
-    });
+    // return res.status(500).json({
+    //   success: false,
+    //   message: "Product not found",
+    // });
+    return next(new ErrorHandler("Product not found", 404)); //you can consider this an error like we plain old Error object just with some added functionalities pf passing some info alog with it, so this error will be caught by err in middleware
   }
 
   res.status(200).json({
@@ -45,11 +48,15 @@ exports.createProduct = async (req, res, next) => {
 exports.updateProduct = async (req, res, next) => {
   let product = Product.findById(req.params.id);
 
+  // if (!product) {
+  //   return res.status(500).json({
+  //     success: false,
+  //     message: "Product no found",
+  //   });
+  // }
+
   if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product no found",
-    });
+    return next(new ErrorHandler("Product not found", 404)); //you can consider this an error like we plain old Error object just with some added functionalities pf passing some info alog with it, so this error will be caught by err in middleware
   }
 
   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
@@ -69,11 +76,15 @@ exports.updateProduct = async (req, res, next) => {
 exports.deleteProduct = async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
+  // if (!product) {
+  //   return res.status(500).json({
+  //     success: false,
+  //     message: "Product not found",
+  //   });
+  // }
+
   if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product not found",
-    });
+    return next(new ErrorHandler("Product not found", 404)); //you can consider this an error like we plain old Error object just with some added functionalities pf passing some info alog with it, so this error will be caught by err in middleware
   }
 
   await product.remove();
