e7e06b92c5b40fc6ecf09db0b13541820c4506f4
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-04-06 18:26:32 +0530

    hashed the password before saving it to the database

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+const bcrypt = require("bcryptjs");
+
 const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
@@ -37,6 +39,14 @@ const userSchema = new mongoose.Schema({
   },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 module.exports = mongoose.model("User", userSchema);
