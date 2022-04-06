9005df4d60b5a57491933f675a338ccacbeeeb3b
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-04-06 18:53:31 +0530

    added functionality to return an authentication token whenever a user is registered/logged in

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const bcrypt = require("bcryptjs");
-
 const validator = require("validator");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -48,5 +48,11 @@ userSchema.pre("save", async function (next) {
 
   this.password = await bcrypt.hash(this.password, 10);
 });
+
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 module.exports = mongoose.model("User", userSchema);
